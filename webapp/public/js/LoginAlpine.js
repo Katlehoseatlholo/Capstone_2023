5315bd19b1018c02ae32a38acd22fb5cc5484b98
Seatlholo4 <katlehoseatlholo4@gmail.com>
2023-09-14 19:05:57 +0200

    Final Fix

--- webapp/public/js/LoginAlpine.js
+++ webapp/public/js/LoginAlpine.js
@@ -1,12 +1,27 @@
-// Wrap the Alpine.js initialization in an async function
+// Define a shared scope to store loggedInUser
+let loggedInUser = JSON.parse(sessionStorage.getItem("loggedInUser")) || {};
 
+// Wrap the Alpine.js initialization in an async function
 document.addEventListener("alpine:init", () => {
+  // Define a new Alpine.js data component called "Login"
   Alpine.data("Login", () => {
     return {
       username: "",
       password: "",
       message: "",
-      loggedInUser: null,
+     loggedInUser: loggedInUser, // Access the shared loggedInUser here
+      updatedUser: {
+        Name: "",
+        Surname: "",
+        MobileNumber: "",
+        AddressLine1: "",
+        Role: "",
+        EmailID: "",
+        Education: "",
+        Experience: "",
+        AdditionalDetails: "",
+      },
+
 
       async register() {
         try {
@@ -47,8 +62,14 @@ document.addEventListener("alpine:init", () => {
                 emailID: this.username, // other user details
               };
 
+              // Store user details in localStorage
+              sessionStorage.setItem(
+                "loggedInUser",
+                JSON.stringify(this.loggedInUser)
+              );
+                 console.log(this.loggedInUser.emailID)
               // Redirect regular users to the profile page
-              window.location.href = "http://localhost:3000/profile.html"; // Change the URL to your profile page
+             window.location.href = "http://localhost:3000/profile.html"; // Change the URL to your profile page
             }
           } else if (response.status === 401) {
             this.message = "Invalid credentials"; // Handle invalid credentials
@@ -71,6 +92,47 @@ document.addEventListener("alpine:init", () => {
           this.message = "Logout failed";
         }
       },
+      async fetchUserDetails() {
+        try {
+          // Check if loggedInUser and EmailID exist before making the request
+          if (this.loggedInUser && this.loggedInUser.emailID) {
+            // Use the correct endpoint for fetching user details
+            const response = await axios.get(`/user/${this.loggedInUser.emailID}`);
+
+            // Debug log: Check if user details are fetched
+            console.log("Fetched User Details:", response.data);
+
+            this.loggedInUser = response.data;
+            this.updatedUser = { ...this.loggedInUser };
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      },
+
+      async saveProfile() {
+        try {
+          // Use the correct endpoint for updating user details
+          await axios.put(`/user/${this.updatedUser.EmailID}`, this.updatedUser);
+          alert("Profile updated successfully");
+        } catch (error) {
+          console.error(error);
+          alert("Failed to update profile");
+        }
+      },
+
+      async getEmail() {
+        try {
+          // Access the email directly from loggedInUser
+          console.log(this.loggedInUser.EmailID);
+        } catch (error) {
+          console.error(error);
+          alert("Failed to get email");
+        }
+      },
+
     };
   });
+
+
 });
--- webapp/public/js/profileAlpine.js
+++ webapp/public/js/profileAlpine.js
@@ -1,43 +1,56 @@
 // Listen for the "alpine:init" event to initialize Alpine.js
 document.addEventListener("alpine:init", () => {
-    // Define a new Alpine.js data component called "Profile"
-    Alpine.data("Profile", () => {
-      return {
-        loggedInUser: {},
-        updatedUser: {
-          Name: "",
-          Surname: "",
-          MobileNumber: "",
-          AddressLine1: "",
-          Role: "",
-          EmailID: "",
-          Education: "",
-          Experience: "",
-          AdditionalDetails: "",
-        },
-  
-        async fetchUserDetails(email) {
-          try {
+  // Define a new Alpine.js data component called "Profile"
+  Alpine.data("Profile", () => {
+    return {
+      loggedInUser: JSON.parse(sessionStorage.getItem("loggedInUser")) || {},
+      updatedUser: {
+        Name: "",
+        Surname: "",
+        MobileNumber: "",
+        AddressLine1: "",
+        Role: "",
+        EmailID: "",
+        Education: "",
+        Experience: "",
+        AdditionalDetails: "",
+      },
+
+      async fetchUserDetails() {
+        try {
+          // Check if loggedInUser and EmailID exist before making the request
+          if (this.loggedInUser && this.loggedInUser.EmailID) {
             // Use the correct endpoint for fetching user details
-            const response = await axios.get(`/user/${email}`);
+            const response = await axios.get(`/user/${this.loggedInUser.EmailID}`);
             this.loggedInUser = response.data;
             this.updatedUser = { ...this.loggedInUser };
-          } catch (error) {
-            console.error(error);
           }
-        },
-  
-        async saveProfile() {
-          try {
-            // Use the correct endpoint for updating user details
-            await axios.put(`/user/${this.updatedUser.EmailID}`, this.updatedUser);
-            alert("Profile updated successfully");
-          } catch (error) {
-            console.error(error);
-            alert("Failed to update profile");
-          }
-        },
-      };
-    });
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      
+
+      async saveProfile() {
+        try {
+          // Use the correct endpoint for updating user details
+          await axios.put(`/user/${this.updatedUser.EmailID}`, this.updatedUser);
+          alert("Profile updated successfully");
+        } catch (error) {
+          console.error(error);
+          alert("Failed to update profile");
+        }
+      },
+      
+      async getEmail() {
+        try {
+          // Use the correct endpoint for updating user details
+          console.log(this.loggedInUser.EmailID);
+        } catch (error) {
+          console.error(error);
+          alert("Failed to get email");
+        }
+      },
+    };
   });
-  
+});
