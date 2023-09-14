7ba984af727cd1a4c8c82eed7a14b0da2a2845f6
Seatlholo4 <katlehoseatlholo4@gmail.com>
2023-09-14 17:17:06 +0200

    user session enabled

--- webapp/index.js
+++ webapp/index.js
@@ -1,119 +1,157 @@
-import express, { urlencoded, json } from 'express';
+import express, { urlencoded, json } from "express";
 import * as sqlite from "sqlite";
 import sqlite3 from "sqlite3";
-import session from 'express-session';
+import session from "express-session";
+import cors from "cors";
+
+// Enable CORS for all routes
 
 const app = express();
 
 // Configure session middleware
-app.use(session({
-  secret: 'key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-const db = await sqlite.open({
-  filename: "./101.db",
-  driver: sqlite3.Database,
-});
-
-await db.migrate();
-
-/////////////////// 
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(express.static('public'))
-
-
-
-// Registration route
-app.post('/register', (req, res) => {
-    const { emailID, password } = req.body;
-  
-    // Insert user data into the database (ensure proper validation and hashing)
-    db.run('INSERT INTO Users (EmailID, Userpassword) VALUES (?, ?)', [emailID, password], (err) => {
-      if (err) {
-        return res.status(500).json({ error: 'Registration failed' });
-      }
-      res.send('Registration successful');
-    });
-  });
-  
-  // Login route
-  app.post('/login', (req, res) => {
-    const { emailID, password } = req.body;
-  
-    // Check user credentials against the database (ensure proper validation and hashing)
-    db.get('SELECT * FROM Users WHERE EmailID = ? AND Userpassword = ?', [emailID, password], (err, user) => {
-      if (err) {
-        return res.status(500).json({ error: 'Login failed' });
-      }
-      if (!user) {
-        return res.status(401).json({ error: 'Invalid credentials' });
-      }
-  
-      // Create a session variable to store user information
-      req.session.user = user;
-  
-      res.send('Login successful');
-    });
-  });
-  
-  // Logout route
-  app.get('/logout', (req, res) => {
-    // Destroy the session to log out the user
-    req.session.destroy((err) => {
-      if (err) {
-        return res.status(500).json({ error: 'Logout failed' });
-      }
-      res.send('Logout successful');
-    });
-  });
-
-  
-
-// Fetch user details by email ID
-app.get('/user/:emailID', async (req, res) => {
-  const { emailID } = req.params;
-
+app.use(
+  session({
+    secret: "key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(cors());
+const initializeDatabase = async () => {
   try {
-    const user = await db.get('SELECT * FROM Users WHERE EmailID = ?', [emailID]);
+    const db = await sqlite.open({
+      filename: "./101.db",
+      driver: sqlite3.Database,
+    });
 
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+    await db.migrate();
+    return db;
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    throw error;
+  }
+};
+
+const startServer = async () => {
+  const db = await initializeDatabase();
+
+  // Express middleware
+  app.use(urlencoded({ extended: true }));
+  app.use(json());
+  app.use(express.static("public"));
+
+  // Registration route
+  app.post("/register", async (req, res) => {
+    const { emailID, password } = req.body;
+
+    try {
+      // Insert user data into the database (ensure proper validation and hashing)
+      await db.run(
+        "INSERT INTO Users (EmailID, Userpassword,VoteCount) VALUES (?, ?,0)",
+        [emailID, password]
+      );
+
+      res.send("Registration successful");
+    } catch (error) {
+      console.error("Error registering user:", error);
+      res.status(500).json({ error: "Registration failed" });
     }
+  });
 
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error fetching user details' });
-  }
-});
+  // Login route
+  app.post("/login", async (req, res) => {
+    const { emailID, password } = req.body;
 
-// Update user details
-app.put('/user/:emailID', async (req, res) => {
-  const { emailID } = req.params;
-  const updatedUser = req.body;
+    try {
+      // Check user credentials against the database (ensure proper validation and hashing)
+      const user = await db.get(
+        "SELECT * FROM Users WHERE EmailID = ? AND Userpassword = ?",
+        [emailID, password]
+      );
 
-  try {
-    // Update the user's details in the database
-    await db.run('UPDATE Users SET FirstName = ?, LastName = ?, MobileNumber = ?, AddressLine1 = ?, Education = ? WHERE EmailID = ?', [
-      updatedUser.FirstName,
-      updatedUser.LastName,
-      updatedUser.MobileNumber,
-      updatedUser.AddressLine1,
-      updatedUser.Education,
-      emailID,
-    ]);
+      if (!user) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
 
-    res.json({ message: 'Profile updated successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error updating user profile' });
-  }
-});
+      // User found, create a session variable
+      req.session.user = user;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+      if (user.emailID === "admin@gmail.com") {
+        // Redirect the admin user to the admin dashboard
+        return res.redirect("http://localhost:3000//admindashboard.html");
+      } else {
+        // Create a session variable to store user information
+        return res.redirect("http://localhost:3000//profile.html");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
+      res.status(500).json({ error: "Login failed" });
+    }
+  });
+  // Logout route
+  app.get("/logout", async (req, res) => {
+    try {
+      // Destroy the session to log out the user
+      req.session.destroy();
+      res.send("Logout successful");
+    } catch (error) {
+      console.error("Error logging out:", error);
+      res.status(500).json({ error: "Logout failed" });
+    }
+  });
+
+  // Fetch user details by email ID
+  app.get("/user/:emailID", async (req, res) => {
+    const { emailID } = req.params;
+
+    try {
+      const user = await db.get("SELECT * FROM Users WHERE EmailID = ?", [
+        emailID,
+      ]);
+
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      res.json(user);
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+      res.status(500).json({ error: "Error fetching user details" });
+    }
+  });
+
+  // Update user details
+  app.put("/user/:emailID", async (req, res) => {
+    const { emailID } = req.params;
+    const updatedUser = req.body;
+
+    try {
+      // Update the user's details in the database
+      await db.run(
+        "UPDATE Users SET FirstName = ?, LastName = ?, MobileNumber = ?, AddressLine1 = ?, Education = ? WHERE EmailID = ?",
+        [
+          updatedUser.FirstName,
+          updatedUser.LastName,
+          updatedUser.MobileNumber,
+          updatedUser.AddressLine1,
+          updatedUser.Education,
+          emailID,
+        ]
+      );
+
+      res.json({ message: "Profile updated successfully" });
+    } catch (error) {
+      console.error("Error updating user profile:", error);
+      res.status(500).json({ error: "Error updating user profile" });
+    }
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer().catch((error) => {
+  console.error("Error starting server:", error);
 });
--- webapp/public/js/LoginAlpine.js
+++ webapp/public/js/LoginAlpine.js
@@ -1,67 +1,75 @@
-// Listen for the "alpine:init" event to initialize Alpine.js
+// Wrap the Alpine.js initialization in an async function
+
 document.addEventListener("alpine:init", () => {
-  // Define a new Alpine.js data component called "login"
   Alpine.data("Login", () => {
     return {
-      // Define data properties here
       username: "",
       password: "",
       message: "",
-      loggedInUser: null, // To store the logged-in user's information
+      loggedInUser: null,
 
-
-      // Define methods for interacting with the server
-      register() {
-        axios
-          .post('/register', {
+      async register() {
+        try {
+          const response = await axios.post("/register", {
             emailID: this.username,
             password: this.password,
-          })
-          .then((response) => {
-            this.message = response.data.message;
-          })
-          .catch((error) => {
-            console.error(error);
-            this.message = 'Registration failed';
+          });
+          this.message = response.data.message;
+
+          if (response.data.message === "Registration successful") {
+            // Redirect to the profile page after successful registration
+            window.location.href = "/profile.html"; // Change the URL to your profile page
+          }
+        } catch (error) {
+          console.error(error);
+          this.message = "Registration failed";
+        }
+      },
+
+      // Alpine.js login function
+
+      async login() {
+        try {
+          const response = await axios.post("/login", {
+            emailID: this.username,
+            password: this.password,
           });
 
-           // After a successful registration, you can update the loggedInUser property
-    this.loggedInUser = { emailID: this.username, /* other user details */ };
- 
-      },
-    
-      login() {
-        axios
-          .post('/login', {
-            emailID: this.username,
-            password: this.password,
-          })
-          .then((response) => {
-            console.log(response);
-            this.message = response.data.message;
-            if (response.data.message === 'Login successful') {
-              // Set the loggedInUser when login is successful
-              this.loggedInUser = { emailID: this.username, /* other user details */ };
+          if (response.status === 200) {
+            // Check if the email is admin@gmail.com
+            if (this.username === "admin@gmail.com") {
+              // Redirect to the admin dashboard
+              window.location.href =
+                "http://localhost:3000/admindashboard.html"; // Change the URL to your admin dashboard
+            } else {
+              // Set the loggedInUser when login is successful for regular users
+              this.loggedInUser = {
+                emailID: this.username, // other user details
+              };
+
+              // Redirect regular users to the profile page
+              window.location.href = "http://localhost:3000/profile.html"; // Change the URL to your profile page
             }
-          })
-          .catch((error) => {
-            console.error(error);
-            this.message = 'Login failed';
-          });
+          } else if (response.status === 401) {
+            this.message = "Invalid credentials"; // Handle invalid credentials
+          } else {
+            this.message = "Login failed"; // Handle other login errors
+          }
+        } catch (error) {
+          console.error(error);
+          this.message = "Login failed";
+        }
       },
-    
-      logout() {
-        this.loggedInUser = null;
 
-        axios
-          .get('/logout')
-          .then((response) => {
-            this.message = response.data.message;
-          })
-          .catch((error) => {
-            console.error(error);
-            this.message = 'Logout failed';
-          });
+      async logout() {
+        this.loggedInUser = null;
+        try {
+          const response = await axios.get("/logout");
+          this.message = response.data.message;
+        } catch (error) {
+          console.error(error);
+          this.message = "Logout failed";
+        }
       },
     };
   });
